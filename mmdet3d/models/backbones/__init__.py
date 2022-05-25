6d5fe8d6f6004deefa96352778cd2dca14f4cf3a
discover304 <hobart.yang@qq.com>
2022-05-25 14:46:32 +0800

    fullfill code

--- mmdet3d/models/backbones/__init__.py
+++ mmdet3d/models/backbones/__init__.py
@@ -1,8 +1,17 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmdet.models.backbones import ResNet
+from mmdet.models.backbones import SSDVGG, HRNet, ResNet, ResNetV1d, ResNeXt
+from .dgcnn import DGCNNBackbone
+from .dla import DLANet
+from .mink_resnet import MinkResNet
+from .multi_backbone import MultiBackbone
+from .nostem_regnet import NoStemRegNet
+from .pointnet2_sa_msg import PointNet2SAMSG
+from .pointnet2_sa_ssg import PointNet2SASSG
 from .second import SECOND
 from .second_info_ver import SECOND_INFO
 
 __all__ = [
-    'ResNet', 'SECOND', 'SECOND_INFO'
+    'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'NoStemRegNet',
+    'SECOND', 'DGCNNBackbone', 'PointNet2SASSG', 'PointNet2SAMSG',
+    'MultiBackbone', 'DLANet', 'MinkResNet', 'SECOND_INFO'
 ]
--- mmdet3d/models/fusion_layers/__init__.py
+++ mmdet3d/models/fusion_layers/__init__.py
@@ -1,9 +1,16 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .coord_transform import (apply_3d_transformation, bbox_2d_transform,
+                              coord_2d_transform)
+from .point_fusion import PointFusion
+from .vote_fusion import VoteFusion
 from .x_net_fusion_layers import (PreFusionCat, 
                                   GetGraphRandom, GetGraphPearson, GetGraphDAG, GetGraphNN, 
                                   FusionNN, FusionSummation, FusionGNN, FusionMarkovGNN,
                                   FusionNeckNN)
 
 __all__ = [
+    'PointFusion', 'VoteFusion', 'apply_3d_transformation',
+    'bbox_2d_transform', 'coord_2d_transform', 
     'PreFusionCat', 
     'GetGraphRandom', 'GetGraphPearson', 'GetGraphDAG', 'GetGraphNN', 
     'FusionNN', 'FusionSummation', 'FusionGNN', 'FusionMarkovGNN',
